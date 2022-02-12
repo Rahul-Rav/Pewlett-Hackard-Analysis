35e97947f1aad320a9273c17a7768dee5ae9f4f9
Rahul Raveendran <95504135+Rahul-Rav@users.noreply.github.com>
2022-02-12 10:28:48 -0500

    Add files via upload

--- Pewlett-Hackard-Analysis/schema.sql
+++ Pewlett-Hackard-Analysis/schema.sql
@@ -49,10 +49,26 @@ CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-		PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
 );
 
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 -- Query for Confirmation
-SELECT * FROM departments;
+SELECT * FROM retirement_info;
